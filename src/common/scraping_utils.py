349134415aaeb993aaed4420cd5dcd104beba4cc
bulldra <bulldra@gmail.com>
2024-08-26 07:20:21 +0900

    基盤バージョンアップ

--- src/common/scraping_utils.py
+++ src/common/scraping_utils.py
@@ -1,4 +1,5 @@
 """スクレイピング関連のユーティリティ"""
+
 import os
 import re
 import urllib
@@ -10,41 +11,68 @@ from bs4 import BeautifulSoup
 Site = namedtuple("Site", ("url", "title", "heading", "content"))
 
 
-def is_allow_scraping(url: str):
-    """スクレイピングできるかどうかの判定"""
-
+def is_allow_scraping(url: str) -> bool:
+    """スクレイピングできるかの判定"""
     blacklist_domain: list[str] = [
-        "twitter.com",
         "speakerdeck.com",
-        "www.youtube.com",
+        "twitter.com",
+        "open.spotify.com",
     ]
     black_list_ext: list[str] = [
         ".pdf",
+        ".zip",
+    ]
+    urlobj: urllib.parse.ParseResult = urllib.parse.urlparse(url)
+    return not (
+        urlobj.netloc in [b"", ""]
+        or urlobj.netloc in blacklist_domain
+        or os.path.splitext(urlobj.path)[1] in black_list_ext
+        or is_image_url(url)
+        or is_youtube_url(url)
+    )
+
+
+def is_image_url(url: str) -> bool:
+    """画像URLかどうかの判定"""
+    image_ext: list[str] = [
         ".jpg",
         ".png",
         ".gif",
         ".jpeg",
-        ".zip",
     ]
-    url_obj: urllib.parse.ParseResult = urllib.parse.urlparse(url)
+    urlobj: urllib.parse.ParseResult = urllib.parse.urlparse(url)
+    return os.path.splitext(urlobj.path)[1] in image_ext
 
-    if url_obj.netloc == b"" or url_obj.netloc == "":
-        return False
-    elif url_obj.netloc in blacklist_domain:
-        return False
-    elif os.path.splitext(url_obj.path)[1] in black_list_ext:
-        return False
-    else:
-        return True
+
+def is_youtube_url(url: str) -> bool:
+    """YouTubeのリンクかどうかの判定"""
+    youtube_url_list: list[str] = [
+        "youtu.be",
+        "www.youtube.com",
+        "m.youtube.com",
+    ]
+    urlobj: urllib.parse.ParseResult = urllib.parse.urlparse(url)
+    return urlobj.netloc in youtube_url_list
+
+
+def scraping_raw(url: str) -> str:
+    """スクレイピングの実施"""
+    headers: dict[str, str] = {
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6\
+) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36",
+    }
+    res = requests.get(url, timeout=(3.0, 8.0), headers=headers)
+    if res.status_code != 200:
+        raise ValueError(
+            f"status code is not 200. status code:{res.status_code} url:{url}"
+        )
+    return res.content
 
 
 def scraping(url: str) -> Site:
     """スクレイピングの実施"""
-    res = requests.get(url, timeout=(3.0, 8.0))
-    if res.status_code != 200:
-        raise ValueError(f"status code is not 200. status code:{res.status_code}")
-
-    soup = BeautifulSoup(res.content, "html.parser")
+    content: str = scraping_raw(url)
+    soup = BeautifulSoup(content, "html.parser")
     title = url
     if soup.title is not None and soup.title.string is not None:
         title = re.sub(r"\n", " ", soup.title.string.strip())
@@ -97,5 +125,4 @@ def scraping(url: str) -> Site:
         heading.append(cr_tag.get_text().strip())
 
     content: str = re.sub(r"[\n\s]+", "\n", soup.get_text())
-
     return Site(url, title, heading, content)
